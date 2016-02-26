50f7d67d53bd9add556ab2a4d3b0a4404217043b
Bryn Mathias <brynmathias@gmail.com>
2016-02-26 14:55:47 +0000

    custom colours for alerts

--- config.js.sh
+++ config.js.sh
@@ -9,6 +9,20 @@ angular.module('config', [])
     'client_id'   : "$CLIENT_ID",
     'gitlab_url'  : "$GITLAB_URL"
 
-    'colors'      : {}
+    'colors'      : {
+      'severity': {
+      'BLOCKING'     : '#D8122A',
+      'ERROR'        : '#EA680F',
+      'DISABLED'     : '#FFBE1E',
+      'unknown'      : '#BA2222',
+      'configuring'  : '#A6ACA8',
+      'waiting'      : '#00AA5A',
+      'ready'        : '#00AA5A',
+      'negotiating'  : '#00AA5A',
+      'authenticated': '#00AA5A',
+      'active'       : '#00A1BC',
+      'security'     : '#333333',
+    },
+        }
   });
 EOF
